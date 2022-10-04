7964d58c61db6d90c6b2f918f15e638c57c29361
HKHOA001 <hoangtrangtkt@gmail.com>
2022-10-04 22:01:13 +0700

    first commit

--- Models/StringProcess.cs
+++ Models/StringProcess.cs
@@ -18,8 +18,7 @@ namespace HDK700.Models
     }
     public string LowerToUpper(string strInput)
     {
-        // gia su co chuoi"tin hoc kinh te"
-        // ket qua: "tin hoc kinh te
+
         string strResult = "";
 
         strInput = strInput.ToLower();
@@ -29,7 +28,7 @@ namespace HDK700.Models
     public string UpperToLower (string strInput)
     {
         string strResult = "";
-        //lay ra ky tu dau tien cua chuoi
+
         string firstCharacter = strInput.Substring(0,1).ToUpper();
 
         strResult = firstCharacter + strInput.Substring(1);
@@ -45,19 +44,33 @@ namespace HDK700.Models
         }
     public string RemoveVietnameseAccents (string strInput)
     {  
-    string[] arr1 = new string[] { "á", "à", "ả", "ã", "ạ", "â", "ấ", "ầ", "ẩ", "ẫ", "ậ", "ă", "ắ", "ằ", "ẳ", "ẵ", "ặ",  
+    string[] arr1 = new string[] { 
+    "á","à","ả","ã","ạ",
+    "â","ấ","ầ","ẩ","ẫ","ậ",
+    "ă","ắ","ằ","ẳ","ẵ","ặ",  
     "đ",  
-    "é","è","ẻ","ẽ","ẹ","ê","ế","ề","ể","ễ","ệ",  
+    "é","è","ẻ","ẽ","ẹ",
+    "ê","ế","ề","ể","ễ","ệ",  
     "í","ì","ỉ","ĩ","ị",  
-    "ó","ò","ỏ","õ","ọ","ô","ố","ồ","ổ","ỗ","ộ","ơ","ớ","ờ","ở","ỡ","ợ",  
-    "ú","ù","ủ","ũ","ụ","ư","ứ","ừ","ử","ữ","ự",  
+    "ó","ò","ỏ","õ","ọ",
+    "ô","ố","ồ","ổ","ỗ","ộ",
+    "ơ","ớ","ờ","ở","ỡ","ợ",  
+    "ú","ù","ủ","ũ","ụ",
+    "ư","ứ","ừ","ử","ữ","ự",  
     "ý","ỳ","ỷ","ỹ","ỵ",};  
-    string[] arr2 = new string[] { "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a",  
+    string[] arr2 = new string[] { 
+    "a","a","a","a","a",
+    "a","a","a","a","a","a",
+    "a","a","a","a","a","a",  
     "d",  
-    "e","e","e","e","e","e","e","e","e","e","e",  
+    "e","e","e","e","e",
+    "e","e","e","e","e","e",  
     "i","i","i","i","i",  
-    "o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o",  
-    "u","u","u","u","u","u","u","u","u","u","u",  
+    "o","o","o","o","o",
+    "o","o","o","o","o","o",
+    "o","o","o","o","o","o",  
+    "u","u","u","u","u",
+    "u","u","u","u","u","u",  
     "y","y","y","y","y",};  
     for (int i = 0; i < arr1.Length; i++)  
     {  
